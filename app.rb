ac607b05a27bd6245985ca68850395c7827b05d0
Andrey Stankevych <job@e-mail.ua>
2016-09-22 17:22:15 +0300

    add form to new.erb

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/new' do
+	erb :new			
+end
+
+post '/new' do
+  erb "Hello World"
 end
